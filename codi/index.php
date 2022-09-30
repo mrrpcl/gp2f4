77a182834b7fd314a40e3e75c371d7bc322561c6
mrrpcl <grup01gp2asixmrrpcl@gmail.com>
2022-09-30 20:20:30 +0200

    Commit 3 - Grup01 - Tercera versió del codi PHP del projecte

--- codi/index.php
+++ codi/index.php
@@ -1,5 +1,13 @@
-<?php
-echo "Hola món<br>";
-echo "Som el grup01<br>";
-echo "Aquesta és l'activitat GP2F4A6.3"
-?>
+<!DOCTYPE html>
+<html lang="ca">
+	<head>
+		<meta charset="utf-8">
+		<title>
+			PÀGINA INICIAL DE L'APLICACIÓ CALCULADORA
+		</title>
+	</head>
+	<body>
+		<p><b>CALCUALDORA ARITMÈTICA</b></p>
+		<a href="suma.html">a) Suma de 2 operands</a>
+	</body>
+<html>
